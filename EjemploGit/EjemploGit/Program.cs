dd5034b9210237b3a77887ff7dabf6bff672acbb
Desarrollador Angel <ingangel89_velazco@hotmail.com>
2021-02-09 10:13:44 -0600

    Actualizando el Proyecto 9 - Febrero

--- EjemploGit/EjemploGit/Program.cs
+++ EjemploGit/EjemploGit/Program.cs
@@ -16,7 +16,7 @@ namespace EjemploGit
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Desarrollador1());
+            Application.Run(new Form1());
         }
     }
 }
